ba73a74fc5cdd3e9289f6b73b553a3b049dbcd02
Abhishek Rana <rana8451@gmail.com>
2019-03-15 10:31:07 +0530

    "Initial Commit"

---
+++ quizSite/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+
+
+# Overriding the default label suffix(":") included by Django forms
+class MyForm(forms.Form):
+    def __init__(self,*args,**kwargs):
+        kwargs.setdefault('label_suffix','')
+        super(MyForm,self).__init__(*args, **kwargs)
+
+
+# Inheriting from MyForm instead of forms.Form
+class LoginForm(MyForm):
+
+    email_attrs = {"placeholder":"abc@example.com","type":"text"}
+    pass_attrs  = {"placeholder":"********"}
+    email       = forms.CharField(widget=forms.TextInput(email_attrs),label='Email')
+    password    = forms.CharField(widget=forms.PasswordInput(pass_attrs),label='Pass')
+
+    def clean_email(self):
+        email   = self.cleaned_data.get("email")
+        return email
+class SignupForm(MyForm):
+
+    name_attrs  = {"placeholder":"Full Name","type":"text"}
+    email_attrs = {"placeholder":"abc@example.com","type":"text"}
+    pass_attrs  = {"placeholder":"********"}
+    name        = forms.CharField(widget=forms.TextInput(name_attrs),label='Name')
+    email       = forms.CharField(widget=forms.TextInput(email_attrs),label='Email')
+    password    = forms.CharField(widget=forms.PasswordInput(pass_attrs),label='Pass')
---
+++ quizSite/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import LoginForm,SignupForm
+
+
+def home_page(request):
+    return render(request, "base.html", {})
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {"form":form}
+    # if request.method == "POST":
+    #     print "Email: ", request.POST.get("email")
+    #     print "Pass: ", request.POST.get("password")
+    if form.is_valid():
+        print "Email",form.cleaned_data.get("email")
+        print "Pass", form.cleaned_data.get("password")
+    return render(request, "login.html", context)
+
+
+
+def signup_page(request):
+    form = SignupForm(request.POST or None)
+    context={"form":form}
+    # if request.method == "POST":
+    #     print "Email: ", request.POST.get("email")
+    #     print "Pass: ", request.POST.get("password")
+    if form.is_valid():
+        print form.cleaned_data
+
+    return render(request, "signup.html", context)
