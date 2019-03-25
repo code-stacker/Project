925513199d7671448965ffe46ab71d12d66e65d3
Abhishek Kumar <abhishek.srivastava16@ymail.com>
2019-03-25 08:42:36 +0530

    Nothing

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,12 +8,12 @@ from quizSite.forms import MyUserForm,MyForm
 # Inheriting from MyForm instead of forms.Form
 class SignupForm(MyUserForm):
 
-    user_attrs  = {"placeholder":"username",'autofocus':''}
-    email_attrs = {"placeholder":"abc@example.com"}
-    pass_attrs  = {"placeholder":"********"}
+    user_attrs  = {"placeholder":"username",'autofocus':'','class':'left'}
+    email_attrs = {"placeholder":"abc@example.com",'class':'left'}
+    pass_attrs  = {"placeholder":"********",'class':'right'}
     
-    first_name        = forms.CharField(widget=forms.TextInput({'placeholder':'First Name','autofocus':'autofocus'}),label='First Name')
-    last_name        = forms.CharField(widget=forms.TextInput({'placeholder':'Last Name'}),label='Last Name')
+    first_name        = forms.CharField(widget=forms.TextInput({'placeholder':'First Name','autofocus':'autofocus', 'class':'left'}),label='First Name')
+    last_name        = forms.CharField(widget=forms.TextInput({'placeholder':'Last Name', 'class':'right'}),label='Last Name')
     username      = forms.CharField(widget=forms.TextInput(user_attrs),label='Username')
     email         = forms.EmailField(widget=forms.TextInput(email_attrs),label='Email')
     password1     = forms.CharField(widget=forms.PasswordInput(pass_attrs),label='Password')
